58225cbe9bd424990ad233919ad1a45bc24deaf1
KamilJava <kamillapka1@gmail.com>
2019-03-22 10:21:51 +0100

    commit

---
+++ src/main/java/model/User.java
@@ -0,0 +1,11 @@
+package model;
+
+public class User {
+
+    private int id_u;
+    private String email;
+    private String password;
+    private String role;
+    private boolean enable;
+    
+}
